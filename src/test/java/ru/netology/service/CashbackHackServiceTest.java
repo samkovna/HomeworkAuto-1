a77ed611fc7205208dca5a7e4a47aade5be1dda3
A. Stefanovich <stefanovich.anastasiya@yandex.ru>
2023-05-07 22:48:06 +0300

    changed testNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,9 +7,37 @@ import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     @Test
-    void calculateRemains() {
+    public void calculateRemains() {
         int amount = 900;
         int expected = 100;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void calculateRemainsIfAmountMultipleThousand() {
+        int amount = 1000;
+        int expected = 0;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void calculateRemainsIfAmountMoreThousand() {
+        int amount = 1999;
+        int expected = 1;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateRemainsIfAmountIsZero() {
+        int amount = 0;
+        int expected = 1000;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
